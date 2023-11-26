4df9cb0a4036e18011a25162886582f88158409b
ViktoriiaKrasko <akiv.co@gmail.com>
2023-11-26 21:51:36 +0200

    Add express w/body-parser

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(
+    `Server is listening on ${port} and starting at http://localhost:${port}`
+  );
+});
