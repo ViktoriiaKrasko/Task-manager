4a53815e37c1da270002de6aed739ca718dd4a5b
ViktoriiaKrasko <akiv.co@gmail.com>
2023-11-27 02:22:38 +0200

    Added example of swagger docs

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -7,7 +7,65 @@ const router = express.Router();
 
 router.use(checkAuth);
 
+/**
+ * @openapi
+ * paths:
+ *   /api/task:
+ *     post:
+ *       tags:
+ *         - Task
+ *       summary: Create a task
+ *       security:
+ *        - basicAuth: []
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 description:
+ *                   type: string
+ *                   default: Do laundry
+ *       responses:
+ *         '201':
+ *           description: Created
+ *         '400':
+ *           description: Bad Request
+ */
+
 router.post('/task', taskController.createTask);
+/**
+ * @openapi
+ * paths:
+ *   /api/task:
+ *     get:
+ *       tags:
+ *         - Task
+ *       summary: Get tasks
+ *       security:
+ *         - basicAuth: []
+ *       responses:
+ *         '200':
+ *           description: Success
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                     description:
+ *                       type: string
+ *                     completed:
+ *                       type: boolean
+ *                     createdBy:
+ *                       type: string
+ *         '400':
+ *           description: Bad request
+ */
 router.get('/task', taskController.getTasksByUserId);
 router.get('/task/:id', taskController.getTask);
 router.put('/task/:id', taskController.updateTask);
